b070bc0c8763796fd0e3de2083acda86ced905d8
Victor Wibisono <devacto@gmail.com>
2014-12-15 13:40:36 +1100

    Got basic clojure to work.

---
+++ pact-jvm-consumer-clojure/src/main/clojure/au/com/dius/pact/consumer/clojure/pact_clojure.clj
@@ -0,0 +1,6 @@
+(ns au.com.dius.pact.consumer.clojure.pact-clojure)
+
+(defn -main
+      "I don't do a whole lot ... yet."
+      [& args]
+      (println "Hello, World!"))
---
+++ pact-jvm-consumer-clojure/src/test/clojure/au/com/dius/pact/consumer/clojure/pact_clojure_test.clj
@@ -0,0 +1,25 @@
+(ns au.com.dius.pact.consumer.clojure.pact-clojure-test
+  (:require [clojure.test :refer :all]
+            [org.httpkit.client :as http]
+            [au.com.dius.pact.consumer.clojure.pact-clojure :refer :all])
+  (:import [au.com.dius.pact.consumer ConsumerPactBuilder ConsumerPactTest]
+           [au.com.dius.pact.model MockProviderConfig]))
+
+(deftest test-test
+  (let [consumer-fragment (-> "test_consumer"
+                              ConsumerPactBuilder/consumer
+                              (.hasPactWith "test_provider")
+                              (.uponReceiving "clojure test interaction")
+                              (.path "/sample")
+                              (.method "GET")
+                              .willRespondWith
+                              (.status 200)
+                              .toFragment)
+        config (MockProviderConfig. 23456 "localhost")]
+    (is (= (ConsumerPactTest/PACT_VERIFIED)
+           (.runConsumer consumer-fragment config
+                         (proxy [TestRun] []
+                           (run [_] (
+                                      #(is (= 200
+                                              (:status
+                                                @(http/get (str (.url config) "/sample")))))))))))))
